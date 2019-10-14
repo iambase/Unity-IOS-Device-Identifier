123fcfadf6a4a98c7bb579bff414e6e4321338bb
iambase <30421546+iambase@users.noreply.github.com>
2019-10-14 15:41:01 +0100

    Adding Demo

---
+++ Demo/Assets/Scripts/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Demo : MonoBehaviour
+{
+
+#pragma warning disable 0649
+    [SerializeField] private Text _text;
+#pragma warning restore 0649
+
+    private void Start()
+    {
+        _text.text = IPhone.Generation.ToString();
+    }
+}
